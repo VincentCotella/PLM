4f3100856e955f45f7655a5995ccc277b9c69a7b
Gabriel C <gabriel.chaiix@gmail.com>
2024-12-03 16:01:29 +0100

    production sites

--- products/forms.py
+++ products/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from .models import Product
+from .models import Product, Site
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['reference', 'name', 'product_range', 'is_modified', 'created_by', 'stl_file']
+        fields = ['reference', 'name', 'product_range', 'site', 'is_modified', 'created_by', 'stl_file']
         widgets = {
-            'stl_file': forms.URLInput(attrs={'placeholder': 'Enter STL file URL'})
+            'stl_file': forms.URLInput(attrs={'placeholder': 'Enter STL file URL'}),
+            'site': forms.Select(attrs={'class': 'form-select mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'})
         }
